d6458679b8ea004b24f609b11e323241886e5bef
OmarCardoze <cardozeomar@gmail.com>
2021-04-13 14:06:58 -0500

    jwt added

---
+++ controllers/login.js
@@ -0,0 +1,48 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../model/User')
+
+loginRouter.post('', async (req, res) => {
+    const { body } = req
+
+    //buscar el usuario
+    const { username, password } = body
+    const user = await User.findOne({ username })
+
+    //verificar la contrasena
+
+    const passwordCorrect = user === null
+        ? false
+        : await bcrypt.compare(password, user.passwordHash)
+
+    if (!(user && passwordCorrect)) {
+        res.status(401).json({
+            error: 'invalid user or password'
+        })
+    }
+
+    //creacion del token 
+
+    const userForToken = {
+        id: user._id,
+        username: user.username,
+    }
+
+    //expiresIn para la experiacion
+    const token = jwt.sign(
+        userForToken,
+        process.env.SECRET,
+        {
+            expiresIn: 60 * 60 * 24 * 7
+        }
+        )
+
+    res.send({
+        name: user.name,
+        username: user.username,
+        token
+    })
+})
+
+module.exports = loginRouter
--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const Note = require('./model/Note')
 const User = require('./model/User')
 const notFound = require('./middleware/notFound')
 const handleError = require('./middleware/handleError')
+const userExtractor = require('./middleware/userExtractor')
 const userRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const { request } = require('express')
 
 app.use(cors())
 app.use(express.json())
@@ -36,7 +39,7 @@ app.get('/api/notes/:id', (req, res, next) => {
     .catch(error => next(error))
 })
 
-app.put('/api/notes/:id', (req, res, next) => {
+app.put('/api/notes/:id', userExtractor, (req, res, next) => {
   const { id } = req.params
   const note = req.body
 
@@ -53,7 +56,7 @@ app.put('/api/notes/:id', (req, res, next) => {
 
 })
 
-app.delete('/api/notes/:id', async (req, res, next) => {
+app.delete('/api/notes/:id',userExtractor, async (req, res, next) => {
   const { id } = req.params
 
   const result = await Note.findByIdAndDelete(id)
@@ -62,12 +65,17 @@ app.delete('/api/notes/:id', async (req, res, next) => {
   res.status(204).end()
 })
 
-app.post('/api/notes', async (req, res, next) => {
+app.post('/api/notes', userExtractor, async (req, res, next) => {
   const {
     content,
-    important = false, 
-    userId
+    important = false
   } = req.body
+
+  //agregar autorizacion al header
+  //doc: https://developer.mozilla.org/en-US/docs/Web/HTTP/Authentication
+
+  // sacar el userId del request
+  const {userId} = req
 
   const user = await User.findById(userId)
 
@@ -82,14 +90,14 @@ app.post('/api/notes', async (req, res, next) => {
     content,
     date: new Date(),
     important,
-    user: user.id
+    user: user._id
   })
 
   try {
     const savedNote = await newNote.save()
 
     // esta forma solo funciona con mongoose y no con mongodb
-    user.notes = user.notes.concat(savedNote.id)
+    user.notes = user.notes.concat(savedNote._id)
     await user.save()
 
     res.json(savedNote)
@@ -99,6 +107,7 @@ app.post('/api/notes', async (req, res, next) => {
 })
 
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(notFound)
 app.use(handleError)
